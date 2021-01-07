71bb69aab479d198d85f5b55f0723ca0c8475e72
mohammed kabashi <67221132+Alkhateem75872@users.noreply.github.com>
2021-01-07 11:30:38 +0200

    Update final project.cpp

--- final project.cpp
+++ final project.cpp
@@ -3,7 +3,6 @@ class :second
 department: computer*/
 
 /*try to start it with other application like codeblock...*/
-
 #include<fstream>
 #include<string>
 #include<iostream>
@@ -139,7 +138,7 @@ void edit_contact()/*function to add modifications to a specific contact*/
 void delete_contact()/*A function to delete a contact*/
 {
 	int num;
-	cout<<endl<<endl<<"Please Enter The contact #: ";
+	cout<<endl<<endl<<"Please Enter The contact that you want to delete #: ";
 	cin>>num;
 	fp.open("contactBook.txt",ios::in|ios::out);
 	fstream fp2;
@@ -156,14 +155,14 @@ void delete_contact()/*A function to delete a contact*/
 bool screen()/*function to show and display main menu screen*/
 {
 	cout << "\n MAIN MENU\n........................ \n(1) Add a new Contact information \n (2) List all Contacts information \n (3) Search for contact information \n (4) edit a Contact information\n (5) Delete a Contact information\n(0)Exit\n.......................|\n\t\t";
-	cout << "\n Enter the  number choice: ";
+	cout << "\n Enter the  number of your  choice: ";
 
 }
 
 int main()/*main function*/
 {
 	int i;
-	cout << "\n\n\t ... Welcome to Contact  System ....";
+	cout << "\n\n\t ... Welcome to Contact  System  information....";
 
 	while(screen())
 	{
@@ -181,7 +180,7 @@ int main()/*main function*/
 			break;
 		case 2:
 			int num;
-			cout << "\n\n\tPhone: ";
+			cout << "\n\n\tPhne: ";
 			cin >> num;
 			display_contact(num);
 			break;
@@ -193,7 +192,7 @@ int main()/*main function*/
 			delete_contact();
 			break;
 		default:
-		cout<<"\nERROR : Wrong Choice! ,Try Again..\n";
+		cout<<"\nERROR : error ,Try Again..\n";
 			break;
 		}
 	}
